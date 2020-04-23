0eafca5d7aac5f90814ded0c149f233fc13335ce
mreyes2000 <mreyes2000@users.noreply.github.com>
2020-04-23 10:37:28 -0500

    final lab

--- lab20.ml
+++ lab20.ml
@@ -1,40 +1,38 @@
-type image = float list list ;;
+type imageType = float list list ;;
 (* images are lists of lists of floats between 0. (white) and 1. (black) *)
 type size = int * int ;;
 open Graphics ;;
   
 (* threshold thershold image -- image where pixels above the threshold
 value are black *)
-let threshold img threshold =
-  List.map  (fun row -> List.map (fun v -> if v <= threshold then 0. else 1.)
-                                 row) img
+let threshold (img : imageType) (t : float) : imageType =
+  let inner_threshold = List.map (fun cond -> if cond <= t then 0. else 1.) in
+  List.map inner_threshold img;;
        
-(* show the image *)
-let depict img =
-  Graphics.open_graph ""; Graphics.clear_graph ();
-  let x, y = List.length (List.hd img), List.length img in Graphics.resize_window x y;
-  let depict_pix v r c = let lvl = int_of_float (255. *. (1. -. v)) in Graphics.set_color (Graphics.rgb lvl lvl lvl);
-  plot c (y - r) in
-  List.iteri (fun r row -> List.iteri (fun c pix -> depict_pix pix r c) row) img;
-  Unix.sleep 2; Graphics.close_graph () ;;
-
 (* dither max image -- dithered image *)
-let dither img =
-  List.map
-    (fun row ->
-     List.map
-       (fun v -> if v > Random.float 1.
-                 then 1.
-                 else 0.) row)
-    img
+let dither (img : imageType) : imageType =
+  let inner_dither= List.map (fun cond -> if cond > Random.float 1. then 1. else 0.) in 
+  List.map inner_dither img;;
+
+let depict (img : imageType) : unit =
+  Graphics.open_graph ""; 
+  Graphics.clear_graph ();
+  let x, y = List.length (List.hd img), List.length img in 
+  Graphics.resize_window x y;
+  let depict_pix (l, w : size) (v : float)  : unit  = 
+    let lvl = int_of_float (255. *. (1. -. v)) in 
+    Graphics.set_color (Graphics.rgb lvl lvl lvl);
+    plot w (y - l) in
+  List.iteri (fun l_iter-> List.iteri (fun w_iter -> depict_pix (l_iter, w_iter))) img;
+  Unix.sleep 2; 
+  Graphics.close_graph () ;;
     
 let mona = Monalisa.image ;;
-  
-  depict mona ;;
+depict mona ;;
     
-  let mona_threshold = threshold mona 0.75 ;;
-    depict mona_threshold ;;
+let mona_threshold = threshold mona 0.75 ;;
+depict mona_threshold ;;
       
-    let mona_dither = dither mona ;;
-      depict mona_dither ;;
+let mona_dither = dither mona ;;
+depict mona_dither ;;
            
